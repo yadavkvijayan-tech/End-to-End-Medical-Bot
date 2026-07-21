23a8057be9608992d37299ce82071e567717b83d
yadav <yadavkvijayan@gmail.com>
2026-07-21 21:46:42 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="generative-ai-project",
+    version="0.0.0",
+    author="Yadav K",
+    author_email="yadavkvijayan@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
